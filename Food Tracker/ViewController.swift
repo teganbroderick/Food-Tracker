f03b2c21e058845350b725659be274647caac544
Tegan Broderick <teganbroderick@gmail.com>
2020-01-14 20:45:45 -0800

    Initial Commit

---
+++ Food Tracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Food Tracker
+//
+//  Created by Tegan Broderick on 1/14/20.
+//  Copyright © 2020 Tegan Broderick. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
