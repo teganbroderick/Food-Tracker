e7d3986b5307e4cac746fb6ada62303c3981d50e
Tegan Broderick <teganbroderick@gmail.com>
2020-01-23 11:50:31 -0800

    Connect the UI to code complete. Added outlets and actions to source code. Added code to update UI as user interacts with app.

--- Food Tracker/ViewController.swift
+++ Food Tracker/ViewController.swift
@@ -8,9 +8,11 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
     
-    //Mark: Properties
+    //By adopting the UITextFieldDelegate protocol, you tell the compiler that the ViewController class can act as a valid text field delegate. This means you can implement the protocol’s methods to handle text input, and you can assign instances of the ViewController class as the delegate of the text field.
+    
+    //MARK: Properties
     @IBOutlet weak var nameTextField: UITextField!
     @IBOutlet weak var mealNameLabel: UILabel!
     //! means the type is an implicitly unwrapped optional
@@ -20,19 +22,37 @@ class ViewController: UIViewController {
     //not yet been set.
     //When a view controller is loaded from a storyboard, the system instantiates the view hierarchy and assigns the appropriate values to all the view controller’s outlets. By the time the view controller’s viewDidLoad() method is called, the system has assigned valid values to all of the controller’s outlets, and you can safely access their contents. //
     
-    //Mark: Actions
+    
+    //MARK: UITextFieldDelegate
+    func textFieldShouldReturn(_ textField: UITextField) -> Bool {
+        // Hide the keyboard.
+        textField.resignFirstResponder()
+        return true
+        //This method returns a Boolean value that indicates whether the system should process the press of the Return key. In this case, you always want to respond to the user pressing the Return key, so just return true.
+    }
+    
+    func textFieldDidEndEditing(_ textField: UITextField) {
+        mealNameLabel.text = textField.text
+    }
+    
+    //MARK: Actions
     @IBAction func setDefaultTextLabel(_ sender: UIButton) {
         mealNameLabel.text = "Default Text"
     }
     //sender is the object responsible for triggering the action
     
-    
-    
-    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        // Handle the text field’s user input through delegate callbacks.
+        nameTextField.delegate = self
+        
+        //The self refers to the ViewController class, because it’s referenced inside the scope of the ViewController class definition.
+        //When a ViewController instance is loaded, it sets itself as the delegate of its nameTextField property.
+        
     }
+    
+    
 
 
 }
